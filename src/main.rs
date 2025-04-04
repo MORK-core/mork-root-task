ab51341f41a55c07ca8f34f40d0ecba30fc590d8
CtrlZ233 <328825619@qq.com>
2025-04-04 17:05:59 +0800

    [refactor] add ipc buffer init module

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,16 @@
 #![no_main]
 #![feature(linkage)]
 
-use mork_common::constants::{CNodeSlot, ObjectType};
+use mork_common::constants::CNodeSlot;
 use mork_common::mork_user_log;
-use mork_common::types::VMRights;
-use mork_user_lib::mork_alloc::mork_alloc_object;
-use mork_user_lib::mork_mm::mork_map_frame_anyway;
 use mork_user_lib::mork_task::mork_thread_suspend;
+use crate::ipc_buffer::get_ipc_buffer;
+
 extern crate alloc;
 mod hal;
 mod lang_item;
 mod heap;
-
-pub const IPC_BUFFER_ADDR: usize = 0x10000000;
+mod ipc_buffer;
 
 #[unsafe(no_mangle)]
 pub fn main() -> () {
@@ -22,31 +20,14 @@ pub fn main() -> () {
     mork_user_log!(info, "Hello, world!");
 
     heap::init();
-    match mork_alloc_object(CNodeSlot::CapInitCNode as usize, ObjectType::Frame) {
-        Ok(frame_handler) => {
-            mork_user_log!(info, "success to allocate memory: {:?}", frame_handler);
-            let vm_rights = VMRights::R | VMRights::W;
-            match mork_map_frame_anyway(
-                CNodeSlot::CapInitCNode as usize,
-                CNodeSlot::CapInitVSpace as usize,
-                frame_handler,
-                IPC_BUFFER_ADDR,
-                vm_rights
-            ) {
-                Ok(_) => {}
-                Err(resp) => {
-                    mork_user_log!(error, "fail to map frame: {:?}", resp);
-                    mork_thread_suspend(CNodeSlot::CapInitThread as usize).unwrap();
-                }
-            }
-
-        }
-        Err(resp) => {
-            mork_user_log!(error, "fail to allocate memory: {:?}", resp);
-            mork_thread_suspend(CNodeSlot::CapInitThread as usize).unwrap();
-        }
+    if let Err(()) = ipc_buffer::init() {
+        mork_user_log!(error, "Failed to initialize IPC context!");
+        mork_thread_suspend(CNodeSlot::CapInitThread as usize).unwrap();
     }
-    mork_user_log!(info, "map ipc buffer successfully!");
-    mork_thread_suspend(CNodeSlot::CapInitThread as usize).unwrap();
 
+    let ipc_buffer = get_ipc_buffer();
+    ipc_buffer.user_data = 100;
+    mork_user_log!(debug, "ipc buffer user data: {}", ipc_buffer.user_data);
+
+    mork_thread_suspend(CNodeSlot::CapInitThread as usize).unwrap();
 }
